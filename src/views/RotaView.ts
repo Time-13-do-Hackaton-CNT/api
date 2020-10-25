76d4a4d46dab0abbd924fd29d265f7ff2c712e49
Caique Pimenta <cpimenta.apsoftware@gmail.com>
2020-10-25 19:38:07 -0300

    refactor(view): change attributes naming

--- src/views/RotaView.ts
+++ src/views/RotaView.ts
@@ -5,7 +5,7 @@ export default {
   render(rota: Rota) {
     return {
       id: rota.rot_id_rota,
-      rota: rota.rot_ds_rota,
+      descricao: rota.rot_ds_rota,
       latitude_origem: rota.rot_vl_latitude_origem,      
       longitude_origem: rota.rot_vl_longitude_origem,
       latitude_destino: rota.rot_vl_latitude_destino,      
--- src/views/VariavelPesquisa.ts
+++ src/views/VariavelPesquisa.ts
@@ -5,7 +5,7 @@ export default {
   render(variavelPesquisa: VariavelPesquisa) {
     return {
       id: variavelPesquisa.vap_id_variavel_pesquisa,
-      variavel: variavelPesquisa.vap_ds_variavel,
+      descricao: variavelPesquisa.vap_ds_variavel,
       foto_obrigatoria: variavelPesquisa.vap_st_foto_obrigatoria,      
       tipo: variavelPesquisa.vap_tp_variavel,            
       dadosColeta: DadoColetaView.renderMany(variavelPesquisa.DadosColeta)

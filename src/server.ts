6b40cac7f1ff7bbf9c69b07876433aa300a48d44
Caique Pimenta <cpimenta.apsoftware@gmail.com>
2020-12-04 16:31:29 -0300

    chore: inclusão do pacote connect-timeout

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,25 @@ import './database/connection';
 import routes from './routes';
 import errorHandler from './errors/handler';
 
-const app = express();
+// var timeout =  require('connect-timeout');
 
+const app = express();
+// app.use(timeout('2000s'));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads/fotos', express.static(path.join(__dirname, '..', 'uploads', 'fotos')))
 app.use(errorHandler);
 
-app.listen(3333);
+
+// app.use(timeout('100s'));
+// app.use(timeout(120000));
+// app.use(haltOnTimedout);
+
+// function haltOnTimedout(req, res, next){
+//   if (!req.timedout) next();
+// }
+
+// var server =
+ app.listen(3333);
+// server.setTimeout(500000);
